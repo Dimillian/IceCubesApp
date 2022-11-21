474d29bbd9435fad6c2f9f41440cee8825062a3b
Matt Bonney <bonneym@gmail.com>
2022-11-21 08:47:10 -0500

    Preview account data

--- Packages/Network/Sources/Network/Models/Account.swift
+++ Packages/Network/Sources/Network/Models/Account.swift
@@ -6,3 +6,8 @@ public struct Account: Codable, Identifiable {
   public let displayName: String
   public let avatar: URL
 }
+
+// MARL: Preview Content
+public extension Account {
+    static let preview: Account = Account(id: "1234567890", username: "@johnm@mastodon.social", displayName: "John Mastodon", avatar: URL(string: "https://files.mastodon.social/accounts/avatars/000/415/403/original/5fd8b8dba26e55f1.png")!)
+}
