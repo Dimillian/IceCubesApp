083c07ce8254d4701c508d6219e07fced264ae42
prajeet shrestha <prajeet.shrestha@gmail.com>
2023-01-02 14:38:05 +0545

    Removed toggle for selecting themes and replaced with picker.

--- Packages/DesignSystem/Sources/DesignSystem/ColorSet.swift
+++ Packages/DesignSystem/Sources/DesignSystem/ColorSet.swift
@@ -1,13 +1,25 @@
 import SwiftUI
-
 public protocol ColorSet {
+  var name: ColorSetName { get }
+  var scheme: ColorScheme { get }
   var tintColor: Color { get set }
   var primaryBackgroundColor: Color { get set }
   var secondaryBackgroundColor: Color { get set }
   var labelColor: Color { get set }
 }
 
-public struct DarkSet: ColorSet {
+public enum ColorScheme: String {
+  case dark, light
+}
+
+public enum ColorSetName: String {
+  case iceCubeDark = "Ice Cube - Dark"
+  case iceCubeLight = "Ice Cube - Light"
+}
+
+public struct IceCubeDark: ColorSet {
+  public var name: ColorSetName = .iceCubeDark
+  public var scheme: ColorScheme = .dark
   public var tintColor: Color = Color(red: 187/255, green: 59/255, blue: 226/255)
   public var primaryBackgroundColor: Color =  Color(red: 16/255, green: 21/255, blue: 35/255)
   public var secondaryBackgroundColor: Color = Color(red: 30/255, green: 35/255, blue: 62/255)
@@ -16,7 +28,9 @@ public struct DarkSet: ColorSet {
   public init() {}
 }
 
-public struct LightSet: ColorSet {
+public struct IceCubeLight: ColorSet {
+  public var name: ColorSetName = .iceCubeLight
+  public var scheme: ColorScheme = .light
   public var tintColor: Color = Color(red: 187/255, green: 59/255, blue: 226/255)
   public var primaryBackgroundColor: Color = .white
   public var secondaryBackgroundColor: Color = Color(hex:0xF0F1F2)
--- Packages/DesignSystem/Sources/DesignSystem/Theme.swift
+++ Packages/DesignSystem/Sources/DesignSystem/Theme.swift
@@ -3,51 +3,46 @@ import SwiftUI
 
 public class Theme: ObservableObject {
   enum ThemeKey: String {
-      case colorScheme, tint, label, primaryBackground, secondaryBackground
-      case avatarPosition
+    case colorScheme, tint, label, primaryBackground, secondaryBackground
+    case avatarPosition
+    case selectedSet, selectedScheme
   }
-
+  
   public enum AvatarPosition: String, CaseIterable {
     case leading, top
-
+    
     public var description: LocalizedStringKey {
       switch self {
-        case .leading:
-          return "Leading"
-        case .top:
-          return "Top"
-        }
-      }
-  }
-    
-  @AppStorage("is_previously_set") var isSet: Bool = false
-  @AppStorage(ThemeKey.colorScheme.rawValue) public var colorScheme: String = "dark" {
-    didSet {
-      if colorScheme == "dark" {
-        setColor(set: DarkSet())
-      } else {
-        setColor(set: LightSet())
+      case .leading:
+        return "Leading"
+      case .top:
+        return "Top"
       }
     }
   }
+  
+  @AppStorage("is_previously_set") private var isSet: Bool = false
+  @AppStorage(ThemeKey.selectedScheme.rawValue) public var selectedScheme: ColorScheme = .dark
   @AppStorage(ThemeKey.tint.rawValue) public var tintColor: Color = .black
   @AppStorage(ThemeKey.primaryBackground.rawValue) public var primaryBackgroundColor: Color = .white
   @AppStorage(ThemeKey.secondaryBackground.rawValue) public var secondaryBackgroundColor: Color = .gray
   @AppStorage(ThemeKey.label.rawValue) public var labelColor: Color = .black
   @AppStorage(ThemeKey.avatarPosition.rawValue) var rawAvatarPosition: String = AvatarPosition.top.rawValue
-
+  @AppStorage(ThemeKey.selectedSet.rawValue) var storedSet: ColorSetName = .iceCubeDark
   @Published public var avatarPosition: AvatarPosition = .top
-
+  @Published public var selectedSet: ColorSetName = .iceCubeDark
+  
   private var cancellables = Set<AnyCancellable>()
-
+  
   public init() {
+    // If theme is never set before set the default store. This should only execute once after install.
     if !isSet {
-      setColor(set: DarkSet())
-      isSet.toggle()
+      setColor(withName: .iceCubeDark)
+      isSet = true
     }
-
+    
     avatarPosition = AvatarPosition(rawValue: rawAvatarPosition) ?? .top
-
+    
     $avatarPosition
       .dropFirst()
       .map(\.rawValue)
@@ -55,12 +50,30 @@ public class Theme: ObservableObject {
         self?.rawAvatarPosition = position
       }
       .store(in: &cancellables)
+    
+    // Workaround, since @AppStorage can't be directly observed
+    $selectedSet
+      .dropFirst()
+      .sink { [weak self] colorSetName in
+        self?.setColor(withName: colorSetName)
+      }
+      .store(in: &cancellables)
   }
   
-  public func setColor(set: ColorSet) {
-    self.tintColor = set.tintColor
-    self.primaryBackgroundColor = set.primaryBackgroundColor
-    self.secondaryBackgroundColor = set.secondaryBackgroundColor
-    self.labelColor = set.labelColor
+  public static var allColorSet: [ColorSet] {
+    [
+      IceCubeDark(),
+      IceCubeLight()
+    ]
+  }
+  
+  public func setColor(withName name: ColorSetName) {
+    let colorSet = Theme.allColorSet.filter { $0.name == name }.first ?? IceCubeDark()
+    self.selectedScheme = colorSet.scheme
+    self.tintColor = colorSet.tintColor
+    self.primaryBackgroundColor = colorSet.primaryBackgroundColor
+    self.secondaryBackgroundColor = colorSet.secondaryBackgroundColor
+    self.labelColor = colorSet.labelColor
+    self.storedSet = name
   }
 }
