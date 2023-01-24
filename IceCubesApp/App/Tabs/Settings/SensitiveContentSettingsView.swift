7605b45c6d8541c59daadaae7745cb2d25aca1a0
Gareth Simpson <g@xurble.org>
2023-01-24 10:48:07 +0000

    Local settings for sensitive content.

---
+++ IceCubesApp/App/Tabs/Settings/SensitiveContentSettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  SensitiveContentSettingsView.swift
+//  IceCubesApp
+//
+//  Created by Gareth Simpson on 24/01/2023.
+//
+
+import SwiftUI
+
+struct SensitiveContentSettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SensitiveContentSettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SensitiveContentSettingsView()
+    }
+}
