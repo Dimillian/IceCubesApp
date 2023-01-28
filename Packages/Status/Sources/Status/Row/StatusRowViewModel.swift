e67ae9eb33b320f6865b40ec1306d2815ee53945
Thomas Ricouard <ricouard77@gmail.com>
2023-01-28 11:09:35 +0100

    Avoid one useless status row change

--- Packages/Status/Sources/Status/Row/StatusRowViewModel.swift
+++ Packages/Status/Sources/Status/Row/StatusRowViewModel.swift
@@ -78,7 +78,9 @@ public class StatusRowViewModel: ObservableObject {
           let url = status.content.statusesURLs.first,
           client.hasConnection(with: url)
     else {
-      isEmbedLoading = false
+      if isEmbedLoading {
+        isEmbedLoading = false
+      }
       return
     }
     do {
