Dimillian/IceCubesApp

commit 5871d13eee8add7205f454f78ee8e409893205e0

Add indicator which language is translated (#455)

Packages/Status/Sources/Status/Row/StatusRowContextMenu.swift
@@ -85,7 +85,12 @@ struct StatusRowContextMenu: View {
           await viewModel.translate(userLang: lang)
         }
       } label: {
-        Label("status.action.translate", systemImage: "captions.bubble")
+        if let statusLanguage = viewModel.status.language,
+           let lanugageName = Locale.current.localizedString(forLanguageCode: statusLanguage) {
+          Label("status.action.translate-from-\(lanugageName)", systemImage: "captions.bubble")
+        } else {
+          Label("status.action.translate", systemImage: "captions.bubble")
+        }
       }
     }
 

Packages/Status/Sources/Status/Row/StatusRowView.swift
@@ -316,7 +316,12 @@ public struct StatusRowView: View {
         if viewModel.isLoadingTranslation {
           ProgressView()
         } else {
-          Text("status.action.translate")
+          if let statusLanguage = status.language,
+             let lanugageName = Locale.current.localizedString(forLanguageCode: statusLanguage) {
+            Text("status.action.translate-from-\(lanugageName)")
+          } else {
+            Text("status.action.translate")
+          }
         }
       }
       .buttonStyle(.borderless)

IceCubesApp/Resources/Localization/de.lproj/Localizable.strings
@@ -292,6 +292,7 @@
 
 // MARK: Package: Status
 "status.action.translate" = "Übersetzen";
+"status.action.translate-from-%@" = "Übersetzen aus %@";
 "status.action.translated-label" = "Übersetzt mit DeepL.com";
 "status.action.bookmark" = "Lesezeichen";
 "status.action.boost" = "Boosten";

IceCubesApp/Resources/Localization/en.lproj/Localizable.strings
@@ -291,6 +291,7 @@
 
 // MARK: Package: Status
 "status.action.translate" = "Translate";
+"status.action.translate-from-%@" = "Translate from %@";
 "status.action.translated-label" = "Translated using DeepL.com";
 "status.action.bookmark" = "Bookmark";
 "status.action.boost" = "Boost";

IceCubesApp/Resources/Localization/es.lproj/Localizable.strings
@@ -292,6 +292,7 @@
 
 // MARK: Package: Status
 "status.action.translate" = "Traducir";
+"status.action.translate-from-%@" = "Traducir desde %@";
 "status.action.translated-label" = "Traducido usando DeepL.com";
 "status.action.bookmark" = "Añadir a marcadores";
 "status.action.boost" = "Retootear";

IceCubesApp/Resources/Localization/fr.lproj/Localizable.strings
@@ -287,6 +287,7 @@
 
 // MARK: Package: Status
 "status.action.translate" = "Traduire";
+"status.action.translate-from-%@" = "Traduire de %@";
 "status.action.translated-label" = "Traduit avec DeepL.com";
 "status.action.bookmark" = "Marquer";
 "status.action.boost" = "Promouvoir";

IceCubesApp/Resources/Localization/it.lproj/Localizable.strings
@@ -293,6 +293,7 @@
 
 // MARK: Package: Status
 "status.action.translate" = "Traduci";
+"status.action.translate-from-%@" = "Traduci da %@";
 "status.action.translated-label" = "Tradotto usando DeepL.com";
 "status.action.bookmark" = "Salva nei segnalibri";
 "status.action.boost" = "Condividi";

IceCubesApp/Resources/Localization/ja.lproj/Localizable.strings
@@ -275,6 +275,7 @@
 
 // MARK: Package: Status
 "status.action.translate" = "翻訳";
+"status.action.translate-from-%@" = "翻訳 %@";
 "status.action.translated-label" = "DeepL.comを使用して翻訳";
 "status.action.bookmark" = "ブックマーク";
 "status.action.boost" = "ブースト";

commit 33a5d837c213d9fb611b2896de07c6ce2e087b2f

Fixed typo variable name introduced with #455

Packages/Status/Sources/Status/Row/StatusRowContextMenu.swift
@@ -86,9 +86,9 @@ struct StatusRowContextMenu: View {
         }
       } label: {
         if let statusLanguage = viewModel.status.language,
-           let lanugageName = Locale.current.localizedString(forLanguageCode: statusLanguage)
+           let languageName = Locale.current.localizedString(forLanguageCode: statusLanguage)
         {
-          Label("status.action.translate-from-\(lanugageName)", systemImage: "captions.bubble")
+          Label("status.action.translate-from-\(languageName)", systemImage: "captions.bubble")
         } else {
           Label("status.action.translate", systemImage: "captions.bubble")
         }

Packages/Status/Sources/Status/Row/StatusRowView.swift
@@ -317,9 +317,9 @@ public struct StatusRowView: View {
           ProgressView()
         } else {
           if let statusLanguage = status.language,
-             let lanugageName = Locale.current.localizedString(forLanguageCode: statusLanguage)
+             let languageName = Locale.current.localizedString(forLanguageCode: statusLanguage)
           {
-            Text("status.action.translate-from-\(lanugageName)")
+            Text("status.action.translate-from-\(languageName)")
           } else {
             Text("status.action.translate")
           }