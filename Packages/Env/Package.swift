380a6acfd1de5eb540f575c3f13096cc9b431d21
Joe Barbere <joe.barbere@gmail.com>
2023-02-14 01:00:32 -0500

    Use semver library for version checks (#843)
    Co-authored-by: Thomas Ricouard <ricouard77@gmail.com>

--- Packages/Env/Package.swift
+++ Packages/Env/Package.swift
@@ -18,6 +18,7 @@ let package = Package(
   dependencies: [
     .package(name: "Models", path: "../Models"),
     .package(name: "Network", path: "../Network"),
+    .package(url: "https://github.com/sersoft-gmbh/semver", from: "3.4.0"),
     .package(url: "https://github.com/evgenyneu/keychain-swift", branch: "master"),
   ],
   targets: [
@@ -26,6 +27,7 @@ let package = Package(
       dependencies: [
         .product(name: "Models", package: "Models"),
         .product(name: "Network", package: "Network"),
+        .product(name: "SemVer", package: "semver"),
         .product(name: "KeychainSwift", package: "keychain-swift"),
       ]
     ),
--- Packages/Env/Sources/Env/CurrentInstance.swift
+++ Packages/Env/Sources/Env/CurrentInstance.swift
@@ -1,6 +1,7 @@
 import Foundation
 import Models
 import Network
+import SemVer
 
 @MainActor
 public class CurrentInstance: ObservableObject {
@@ -10,27 +11,21 @@ public class CurrentInstance: ObservableObject {
 
   public static let shared = CurrentInstance()
 
-  private var version: Float {
-    if let stringVersion = instance?.version {
-      if stringVersion.utf8.count > 2 {
-        return Float(stringVersion.prefix(3)) ?? 0
-      } else {
-        return Float(stringVersion.prefix(1)) ?? 0
-      }
-    }
-    return 0
+  private var version: Version {
+    let stringVersion = instance?.version ?? "0"
+    return Version(stringVersion)!
   }
 
   public var isFiltersSupported: Bool {
-    version >= 4
+    version >= Version("4")!
   }
 
   public var isEditSupported: Bool {
-    version >= 4
+    version >= Version("4")!
   }
 
   public var isEditAltTextSupported: Bool {
-    version >= 4.1
+    version >= Version("4.1")!
   }
 
   private init() {}
