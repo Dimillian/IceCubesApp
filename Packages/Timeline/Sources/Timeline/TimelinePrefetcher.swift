7c80dde37389dbf1ee9641630557c663dca21e56
kean <grebenyuk.alexander@gmail.com>
2023-02-18 11:52:11 -0500

    Add image prefectch to the timeline

---
+++ Packages/Timeline/Sources/Timeline/TimelinePrefetcher.swift
@@ -0,0 +1,35 @@
+import SwiftUI
+import UIKit
+import Models
+import Nuke
+
+final class TimelinePrefetcher: NSObject, ObservableObject, UICollectionViewDataSourcePrefetching {
+  private let prefetcher = ImagePrefetcher()
+
+  weak var viewModel: TimelineViewModel?
+
+  func collectionView(_ collectionView: UICollectionView, prefetchItemsAt indexPaths: [IndexPath]) {
+    let imageURLs = getImageURLs(for: indexPaths)
+    prefetcher.startPrefetching(with: imageURLs)
+  }
+
+    func collectionView(_ collectionView: UICollectionView, cancelPrefetchingForItemsAt indexPaths: [IndexPath]) {
+    let imageURLs = getImageURLs(for: indexPaths)
+    prefetcher.stopPrefetching(with: imageURLs)
+  }
+
+  private func getImageURLs(for indexPaths: [IndexPath]) -> [URL] {
+    guard let viewModel, case .display(let statuses, _) = viewModel.statusesState else {
+      return []
+    }
+    return indexPaths.compactMap {
+      $0.row < statuses.endIndex ? statuses[$0.row] : nil
+    }.flatMap(getImages)
+  }
+}
+
+private func getImages(for status: Status) -> [URL] {
+  status.mediaAttachments.compactMap {
+    $0.supportedType == .image ? $0.url : nil
+  }
+}
