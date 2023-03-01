bd1593a1077b863bbc0d54b8bf48a04074d49c91
Gareth Simpson <g@xurble.org>
2023-03-01 05:46:55 +0000

    Record quote toots that have failed to load so that we don't try and load them again. (#1119)
    * Record quote toots that have failed to load so that we don't try and load them again.
    
    Fixes
    1: Repeated visible insertion and removal of placeholder quote toot.
    2: Link hijacking of inline status viewer allowing links to be followed as regular URLs
    
    * Move set
    
    * Add back to routeur check
    
    ---------
    
    Co-authored-by: Thomas Ricouard <ricouard77@gmail.com>

---
+++ Packages/Env/Sources/Env/StatusEmbedCache.swift
@@ -0,0 +1,22 @@
+import Foundation
+import Models
+import SwiftUI
+
+@MainActor
+public class StatusEmbedCache {
+  public static let shared = StatusEmbedCache()
+
+  private var cache: [URL: Status] = [:]
+  
+  public var badStatusesURLs = Set<URL>()
+
+  private init() {}
+
+  public func set(url: URL, status: Status) {
+    cache[url] = status
+  }
+
+  public func get(url: URL) -> Status? {
+    cache[url]
+  }
+}
