aa120a48f7263c9622981b2e18fcbb2a2c411bcf
Thomas Ricouard <ricouard77@gmail.com>
2023-03-13 13:42:32 +0100

    ServerError: Fix

--- Packages/Models/Sources/Models/ServerError.swift
+++ Packages/Models/Sources/Models/ServerError.swift
@@ -2,7 +2,7 @@ import Foundation
 
 public struct ServerError: Decodable, Error {
   public let error: String?
-  public var httpCode: Int
+  public var httpCode: Int?
 }
 
 extension ServerError: Sendable {}
