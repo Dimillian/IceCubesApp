7618af37e49d718c7122cdbafb9a346b232da5a0
Chris Kolbu <chris.kolbu@ittybittyapps.com>
2023-03-20 04:16:15 +1100

    Inline StatusRowView accessibilityLabel modifier
    By inlining this label, we avoid the creation of `ConditionalContent`, which often leads to views being recreated unnecessarily.
    
    In focused mode, the empty label is not read as, it is the accessibility container label for the post component elements.

--- Packages/Status/Sources/Status/Row/StatusRowView.swift
+++ Packages/Status/Sources/Status/Row/StatusRowView.swift
@@ -110,7 +110,12 @@ public struct StatusRowView: View {
                          bottom: 12,
                          trailing: .layoutPadding))
     .accessibilityElement(children: viewModel.isFocused ? .contain : .combine)
-    .modifier(ConditionalAccessibilityLabelModifier(viewModel: viewModel, setLabel: viewModel.isFocused == false))
+    .accessibilityLabel(viewModel.isFocused == false ? CombinedAccessibilityLabel(viewModel: viewModel).finalLabel() : Text(""))
+    .accessibilityCustomContent(
+      LocalizedStringKey("accessibility.status.spoiler-full-content"),
+      viewModel.finalStatus.content.asRawText,
+      importance: .high
+    )
     .accessibilityAction {
       viewModel.navigateToDetail()
     }
@@ -202,11 +207,10 @@ public struct StatusRowView: View {
   }
 }
 
-/// A ``ViewModifier`` that creates a suitable combined accessibility label for a `StatusRowView` that is not focused.
+/// A utility that creates a suitable combined accessibility label for a `StatusRowView` that is not focused.
 @MainActor
-private struct ConditionalAccessibilityLabelModifier: ViewModifier {
+private struct CombinedAccessibilityLabel {
   let viewModel: StatusRowViewModel
-  let setLabel: Bool
 
   var hasSpoiler: Bool {
     viewModel.displaySpoiler && viewModel.finalStatus.spoilerText.asRawText.isEmpty == false
@@ -223,27 +227,7 @@ private struct ConditionalAccessibilityLabelModifier: ViewModifier {
     viewModel.status.reblog != nil
   }
 
-  func body(content: Content) -> some View {
-    if UIAccessibility.isVoiceOverRunning == true, setLabel {
-      if hasSpoiler {
-        // Use the spoiler text in the label and place the full text as custom content
-        content
-          .accessibilityLabel(combinedAccessibilityLabel())
-          .accessibilityCustomContent(
-            LocalizedStringKey("accessibility.status.spoiler-full-content"),
-            viewModel.finalStatus.content.asRawText,
-            importance: .high
-          )
-      } else {
-        content
-          .accessibilityLabel(combinedAccessibilityLabel())
-      }
-    } else {
-      content
-    }
-  }
-
-  func combinedAccessibilityLabel() -> Text {
+  func finalLabel() -> Text {
     userNamePreamble() +
       Text(hasSpoiler
         ? viewModel.finalStatus.spoilerText.asRawText
