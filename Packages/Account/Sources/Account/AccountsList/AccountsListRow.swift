e5f540f98961630dcae3cf92ed80bf96eec23795
Paul Schuetz <pa.schuetz@web.de>
2023-04-07 09:40:03 +0200

    Show verified URLs in account lists
    This allows the user to quickly assess which account of multiple is the official
    one, especially when searching for a person with multiple search results.
    Fixes #1361
    
    Signed-off-by: Paul Schuetz <pa.schuetz@web.de>

--- Packages/Account/Sources/Account/AccountsList/AccountsListRow.swift
+++ Packages/Account/Sources/Account/AccountsList/AccountsListRow.swift
@@ -58,6 +58,31 @@ public struct AccountsListRow: View {
           .environment(\.openURL, OpenURLAction { url in
             routerPath.handle(url: url)
           })
+          
+          let fields = viewModel.account.fields.filter({$0.verifiedAt != nil})
+          
+          if !fields.isEmpty {
+              Capsule()
+                  .frame(height: 1)
+                  .foregroundStyle(.regularMaterial)
+                  .padding(.vertical, 5)
+                  .padding(.horizontal)
+              
+              Text("account-list.verified-fields")
+                  .font(.scaledSubheadline)
+              
+              ForEach(fields) { field in
+                  EmojiTextApp(field.value, emojis: viewModel.account.emojis)
+                      .font(.scaledFootnote)
+                      .emojiSize(Font.scaledFootnoteFont.emojiSize)
+                      .emojiBaselineOffset(Font.scaledFootnoteFont.emojiBaselineOffset)
+                      .environment(\.openURL, OpenURLAction { url in
+                          routerPath.handle(url: url)
+                      })
+                      .padding(.top, 5)
+              }
+          }
+          
         if isFollowRequest {
           FollowRequestButtons(account: viewModel.account,
                                requestUpdated: requestUpdated)
