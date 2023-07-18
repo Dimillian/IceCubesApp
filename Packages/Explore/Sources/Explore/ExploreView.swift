47cf849f2122045bd4a7a8494210b83561d5b978
Thomas Ricouard <ricouard77@gmail.com>
2023-07-18 08:52:10 +0200

    Add scope bar for search in explore screen

--- Packages/Explore/Sources/Explore/ExploreView.swift
+++ Packages/Explore/Sources/Explore/ExploreView.swift
@@ -84,6 +84,11 @@ public struct ExploreView: View {
     .searchable(text: $viewModel.searchQuery,
                 placement: .navigationBarDrawer(displayMode: .always),
                 prompt: Text("explore.search.prompt"))
+    .searchScopes($viewModel.searchScope) {
+      ForEach(ExploreViewModel.SearchScope.allCases, id: \.self) { scope in
+        Text(scope.localizedString)
+      }
+    }
   }
   
   private var quickAccessView: some View {
@@ -121,7 +126,7 @@ public struct ExploreView: View {
 
   @ViewBuilder
   private func makeSearchResultsView(results: SearchResults) -> some View {
-    if !results.accounts.isEmpty {
+    if !results.accounts.isEmpty && (viewModel.searchScope == .all || viewModel.searchScope == .people) {
       Section("explore.section.users") {
         ForEach(results.accounts) { account in
           if let relationship = results.relationships.first(where: { $0.id == account.id }) {
@@ -131,7 +136,7 @@ public struct ExploreView: View {
         }
       }
     }
-    if !results.hashtags.isEmpty {
+    if !results.hashtags.isEmpty && (viewModel.searchScope == .all || viewModel.searchScope == .hashtags) {
       Section("explore.section.tags") {
         ForEach(results.hashtags) { tag in
           TagRowView(tag: tag)
@@ -140,7 +145,7 @@ public struct ExploreView: View {
         }
       }
     }
-    if !results.statuses.isEmpty {
+    if !results.statuses.isEmpty && (viewModel.searchScope == .all || viewModel.searchScope == .posts) {
       Section("explore.section.posts") {
         ForEach(results.statuses) { status in
           StatusRowView(viewModel: { .init(status: status, client: client, routerPath: routerPath) })
