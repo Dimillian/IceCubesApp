63946e9803285ce89e061412dc0b45d68a19eb10
Alessio Maffeis <alessio.maffeis@me.com>
2023-07-20 14:24:26 +0200

    Add newline

--- Packages/Models/Sources/Models/Status.swift
+++ Packages/Models/Sources/Models/Status.swift
@@ -135,7 +135,7 @@ public final class Status: AnyStatus, Codable, Identifiable, Equatable, Hashable
 
   public static func placeholder(forSettings: Bool = false, language: String? = nil) -> Status {
     .init(id: UUID().uuidString,
-          content: .init(stringValue: "Here's to the [#crazy](#) ones. The misfits. The [@rebels](#). The troublemakers.",
+          content: .init(stringValue: "Here's to the [#crazy](#) ones. The misfits.\nThe [@rebels](#). The troublemakers.",
                          parseMarkdown: forSettings),
 
           account: .placeholder(),
