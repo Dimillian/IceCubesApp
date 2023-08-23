30f9da06c838842f9b6aa2922e84ff3cba4e4e6e
Grant McSheffrey <gmcsheff@gmail.com>
2023-08-23 01:08:12 -0400

    Support status links with non-ASCII characters (Bugfix 1546) (#1550)
    * Allow creation of URL objects from strings containing non-ASCII characters
    
    Adds a new initializer for creating URL objects with a flag to specify that
    non-ASCII characters found in the path or query string should first be
    URL encoded.
    
    * Add basic test for creating HTMLString objects
    
    * Encode link paths and queries when parsing statuses
    
    It's common to use non-ASCII characters in URLs even though they're technically
    invalid characters. Every modern browser handles this by silently encoding
    the invalid characters on the user's behalf. However, trying to create a URL
    object with un-encoded characters will result in nil so we need to encode the
    invalid characters before creating the URL object. The unencoded version
    should still be shown in the displayed status.
    
    The parsing of the URL string is a little messy because we can't use the URL
    class for this scenario and need to duplicate some of its work.
    
    * Only encode link URLs as a backup
    
    If a URL can be created from a status href, don't try URL encoding
    it as this could result in double encoding. Only encode the string
    if the creation of a URL fails. This is also more efficient.

--- Packages/Models/Sources/Models/Alias/HTMLString.swift
+++ Packages/Models/Sources/Models/Alias/HTMLString.swift
@@ -11,7 +11,7 @@ public struct HTMLString: Codable, Equatable, Hashable, @unchecked Sendable {
   public var asMarkdown: String = ""
   public var asRawText: String = ""
   public var statusesURLs = [URL]()
-  public private(set) var links = [Link]()
+  private(set) public var links = [Link]()
 
   public var asSafeMarkdownAttributedString: AttributedString = .init()
   private var main_regex: NSRegularExpression?
@@ -151,14 +151,25 @@ public struct HTMLString: Codable, Equatable, Hashable, @unchecked Sendable {
           handleNode(node: nn)
         }
         let finish = asMarkdown.endIndex
+
+        var linkRef = href
+        
+        // Try creating a URL from the string. If it fails, try URL encoding
+        //   the string first.
+        var url = URL(string: href)
+        if url == nil {
+          url = URL(string: href, encodePath: true)
+        }
+        if let linkUrl = url  {
+          linkRef = linkUrl.absoluteString
+          let displayString = asMarkdown[start..<finish]
+          links.append(Link(linkUrl, displayString: String(displayString)))
+        }
+
         asMarkdown += "]("
-        asMarkdown += href
+        asMarkdown += linkRef
         asMarkdown += ")"
 
-        if let url = URL(string: href) {
-          let displayString = asMarkdown[start ..< finish]
-          links.append(Link(url, displayString: String(displayString)))
-        }
         return
       } else if node.nodeName() == "#text" {
         var txt = node.description
@@ -190,19 +201,19 @@ public struct HTMLString: Codable, Equatable, Hashable, @unchecked Sendable {
       self.displayString = displayString
 
       switch displayString.first {
-      case "@":
-        type = .mention
-        title = displayString
-      case "#":
-        type = .hashtag
-        title = String(displayString.dropFirst())
-      default:
-        type = .url
-        var hostNameUrl = url.host ?? url.absoluteString
-        if hostNameUrl.hasPrefix("www.") {
-          hostNameUrl = String(hostNameUrl.dropFirst(4))
-        }
-        title = hostNameUrl
+        case "@":
+          self.type = .mention
+          self.title = displayString
+        case "#":
+          self.type = .hashtag
+          self.title = String(displayString.dropFirst())
+        default:
+          self.type = .url
+          var hostNameUrl = url.host ?? url.absoluteString
+          if hostNameUrl.hasPrefix("www.") {
+            hostNameUrl = String(hostNameUrl.dropFirst(4))
+          }
+          self.title = hostNameUrl
       }
     }
 
@@ -211,5 +222,47 @@ public struct HTMLString: Codable, Equatable, Hashable, @unchecked Sendable {
       case mention
       case hashtag
     }
+  }
+}
+
+extension URL {
+  
+  // It's common to use non-ASCII characters in URLs even though they're technically
+  //   invalid characters. Every modern browser handles this by silently encoding
+  //   the invalid characters on the user's behalf. However, trying to create a URL
+  //   object with un-encoded characters will result in nil so we need to encode the
+  //   invalid characters before creating the URL object. The unencoded version
+  //   should still be shown in the displayed status.
+  public init?(string: String, encodePath: Bool) {
+    var encodedUrlString = ""
+    if encodePath,
+       string.starts(with: "http://") || string.starts(with: "https://"),
+       var startIndex = string.firstIndex(of: "/")
+    {
+      startIndex = string.index(startIndex, offsetBy: 1)
+      
+      // We don't want to encode the host portion of the URL
+      if var startIndex = string[startIndex...].firstIndex(of: "/") {
+        encodedUrlString = String(string[...startIndex])
+        while let endIndex = string[string.index(after: startIndex)...].firstIndex(of: "/") {
+          let componentStartIndex = string.index(after: startIndex)
+          encodedUrlString = encodedUrlString + (string[componentStartIndex...endIndex].addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) ?? "")
+          startIndex = endIndex
+        }
+        
+        // The last part of the path may have a query string appended to it
+        let componentStartIndex = string.index(after: startIndex)
+        if let queryStartIndex = string[componentStartIndex...].firstIndex(of: "?") {
+          encodedUrlString = encodedUrlString + (string[componentStartIndex..<queryStartIndex].addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) ?? "")
+          encodedUrlString = encodedUrlString + (string[queryStartIndex...].addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? "")
+        } else {
+          encodedUrlString = encodedUrlString + (string[componentStartIndex...].addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) ?? "")
+        }
+      }
+    }
+    if encodedUrlString.isEmpty {
+      encodedUrlString = string
+    }
+    self.init(string: encodedUrlString)
   }
 }
