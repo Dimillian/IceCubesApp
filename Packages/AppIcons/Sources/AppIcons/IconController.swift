dce7d4cf17a3c80ac6a33fb62844de1938d1cb42
Matt Bonney <bonneym@gmail.com>
2023-10-17 19:08:21 -0400

    Simple Observable + Environment setup for logic handling

---
+++ Packages/AppIcons/Sources/AppIcons/IconController.swift
@@ -0,0 +1,38 @@
+import SwiftUI
+import Observation
+
+@Observable
+public class IconController {
+    public private(set) var currentIcon: String = UIApplication.shared.alternateIconName ?? AppIcon.primary.appIconName
+
+    private init() {
+    }
+
+    public static let shared = IconController()
+
+    public func setAppIcon(_ newAppIcon: AppIcon) {
+        currentIcon = newAppIcon.appIconName // UIApplication.shared.alternateIconName ?? AppIcon.primary.appIconName
+
+        switch newAppIcon {
+            case .primary:
+                UIApplication.shared.setAlternateIconName(nil)
+            default:
+                UIApplication.shared.setAlternateIconName(newAppIcon.appIconName) { error in
+                    if let error {
+                        print("\(error.localizedDescription) – Icon Name: \(newAppIcon.iconName)")
+                    }
+                }
+        }
+    }
+}
+
+struct IconControllerEnvironmentKey: EnvironmentKey {
+    static let defaultValue: IconController = IconController.shared
+}
+
+public extension EnvironmentValues {
+    var iconController: IconController {
+        get { self[IconControllerEnvironmentKey.self] }
+        set { self[IconControllerEnvironmentKey.self] = newValue }
+    }
+}
