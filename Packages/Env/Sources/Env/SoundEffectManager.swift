2cc13578e06d4b4909181003ef621f00f5960a85
Théo Arrouye <5017270+tarrouye@users.noreply.github.com>
2023-11-06 16:47:35 -0800

    Remove unnecessary vars and switches

--- Packages/Env/Sources/Env/SoundEffectManager.swift
+++ Packages/Env/Sources/Env/SoundEffectManager.swift
@@ -11,14 +11,7 @@ public class SoundEffectManager {
     case pull, refresh, tootSent, tabSelection, bookmark, boost, favorite, share
   }
 
-  var pullId: SystemSoundID = 0
-  var refreshId: SystemSoundID = 1
-  var tootSentId: SystemSoundID = 2
-  var tabSelectionId: SystemSoundID = 3
-  var bookmarkId: SystemSoundID = 4
-  var boostId: SystemSoundID = 5
-  var favoriteId: SystemSoundID = 6
-  var shareId: SystemSoundID = 7
+  private var systemSoundIDs: [SoundEffect: SystemSoundID] = [:]
 
   private let userPreferences = UserPreferences.shared
 
@@ -27,49 +20,26 @@ public class SoundEffectManager {
   }
 
   private func registerSounds() {
-    for effect in SoundEffect.allCases {
-      if let url = Bundle.main.url(forResource: effect.rawValue, withExtension: "wav") {
-        switch effect {
-        case .pull:
-          AudioServicesCreateSystemSoundID(url as CFURL, &pullId)
-        case .refresh:
-          AudioServicesCreateSystemSoundID(url as CFURL, &refreshId)
-        case .tootSent:
-          AudioServicesCreateSystemSoundID(url as CFURL, &tootSentId)
-        case .tabSelection:
-          AudioServicesCreateSystemSoundID(url as CFURL, &tabSelectionId)
-        case .bookmark:
-          AudioServicesCreateSystemSoundID(url as CFURL, &bookmarkId)
-        case .boost:
-          AudioServicesCreateSystemSoundID(url as CFURL, &boostId)
-        case .favorite:
-          AudioServicesCreateSystemSoundID(url as CFURL, &favoriteId)
-        case .share:
-          AudioServicesCreateSystemSoundID(url as CFURL, &shareId)
-        }
-      }
+    SoundEffect.allCases.forEach { effect in
+      guard let url = Bundle.main.url(forResource: effect.rawValue, withExtension: "wav") else { return }
+      register(url: url, for: effect)
     }
   }
 
+  private func register(url: URL, for effect: SoundEffect) {
+    var soundId: SystemSoundID = .init()
+    AudioServicesCreateSystemSoundID(url as CFURL, &soundId)
+    systemSoundIDs[effect] = soundId
+  }
+
   public func playSound(of type: SoundEffect) {
-    guard userPreferences.soundEffectEnabled else { return }
-    switch type {
-    case .pull:
-      AudioServicesPlaySystemSound(pullId)
-    case .refresh:
-      AudioServicesPlaySystemSound(refreshId)
-    case .tootSent:
-      AudioServicesPlaySystemSound(tootSentId)
-    case .tabSelection:
-      AudioServicesPlaySystemSound(tabSelectionId)
-    case .bookmark:
-      AudioServicesPlaySystemSound(bookmarkId)
-    case .boost:
-      AudioServicesPlaySystemSound(boostId)
-    case .favorite:
-      AudioServicesPlaySystemSound(favoriteId)
-    case .share:
-      AudioServicesPlaySystemSound(shareId)
+    guard
+      userPreferences.soundEffectEnabled,
+      let soundId = systemSoundIDs[type]
+    else {
+      return
     }
+
+    AudioServicesPlaySystemSound(soundId)
   }
 }
