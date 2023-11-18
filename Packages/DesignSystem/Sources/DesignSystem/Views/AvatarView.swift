04491ea3a61da96978e42d168cd9f5efd1c48f0a
Duong Thai <dvthai.dev@gmail.com>
2023-11-18 10:34:18 +0700

    refactor shape and placeholder of avatar

--- Packages/DesignSystem/Sources/DesignSystem/Views/AvatarView.swift
+++ Packages/DesignSystem/Sources/DesignSystem/Views/AvatarView.swift
@@ -8,6 +8,49 @@ public struct AvatarView: View {
   @Environment(\.redactionReasons) private var reasons
   @Environment(Theme.self) private var theme
 
+  public let url: URL?
+  public let config: FrameConfig
+
+  public var body: some View {
+    if reasons == .placeholder { placeholder } else { avatarImage }
+  }
+
+  private var avatarImage: some View {
+    LazyImage(request: url.map {
+      ImageRequest(url: $0, processors: [.resize(size: config.size)])
+    }) { state in
+      if let image = state.image {
+        image.resizable().scaledToFill()
+      } else {
+        RoundedRectangle(cornerRadius: cornerRadius).fill(.gray)
+      }
+    }
+    .frame(width: config.width, height: config.height)
+    .clipShape(RoundedRectangle(cornerRadius: cornerRadius))
+    .overlay(
+      RoundedRectangle(cornerRadius: cornerRadius)
+        .stroke(.primary.opacity(0.25), lineWidth: 1)
+    )
+  }
+
+  private var placeholder: some View {
+    RoundedRectangle(cornerRadius: cornerRadius).fill(.gray)
+      .frame(width: config.width, height: config.height)
+  }
+
+  private var cornerRadius: CGFloat {
+    if config == .badge || theme.avatarShape == .circle {
+      return config.width / 2
+    } else {
+      return config.cornerRadius
+    }
+  }
+
+  public init(url: URL?, config: FrameConfig = FrameConfig.status) {
+    self.url = url
+    self.config = config
+  }
+
   public struct FrameConfig: Equatable {
     public let size: CGSize
     public var width: CGFloat { size.width }
@@ -18,7 +61,6 @@ public struct AvatarView: View {
       self.size = CGSize(width: width, height: height)
       self.cornerRadius = cornerRadius
     }
-
 
     public static let account = FrameConfig(width: 80, height: 80)
     public static let status = {
@@ -32,75 +74,32 @@ public struct AvatarView: View {
     public static let list = FrameConfig(width: 20, height: 20, cornerRadius: 10)
     public static let boost = FrameConfig(width: 12, height: 12, cornerRadius: 6)
   }
-
-  public let url: URL?
-  public let config: FrameConfig
-
-  public init(url: URL?, config: FrameConfig = FrameConfig.status) {
-    self.url = url
-    self.config = config
-  }
-
-  public var body: some View {
-    Group {
-      if reasons == .placeholder {
-        RoundedRectangle(cornerRadius: config.cornerRadius)
-          .fill(.gray)
-          .frame(width: config.width, height: config.height)
-      } else {
-        LazyImage(request: url.map { makeImageRequest(for: $0) }) { state in
-          if let image = state.image {
-            image
-              .resizable()
-              .scaledToFill()
-          } else {
-            AvatarPlaceholderView(config: config)
-          }
-        }
-        .frame(width: config.width, height: config.height)
-      }
-    }
-    .clipShape(clipShape)
-    .overlay(
-      clipShape.stroke(Color.primary.opacity(0.25), lineWidth: 1)
-    )
-  }
-
-  private func makeImageRequest(for url: URL) -> ImageRequest {
-    ImageRequest(url: url, processors: [.resize(size: config.size)])
-  }
-
-  private var clipShape: some Shape {
-    switch theme.avatarShape {
-    case .circle:
-      AnyShape(Circle())
-    case .rounded:
-      AnyShape(RoundedRectangle(cornerRadius: config.cornerRadius))
-    }
-  }
-}
-
-private struct AvatarPlaceholderView: View {
-  let config: AvatarView.FrameConfig
-
-  var body: some View {
-    if config == .badge {
-      Circle()
-        .fill(.gray)
-        .frame(width: config.width, height: config.height)
-    } else {
-      RoundedRectangle(cornerRadius: config.cornerRadius)
-        .fill(.gray)
-        .frame(width: config.width, height: config.height)
-    }
-  }
 }
 
 struct AvatarView_Previews: PreviewProvider {
   static var previews: some View {
-    AvatarView(url: url, config: .account)
-      .environment(Theme.shared)
+    PreviewWrapper()
+      .padding()
       .previewLayout(.sizeThatFits)
+  }
+
+}
+
+struct PreviewWrapper: View {
+  @State private var isCircleAvatar = false
+
+  var body: some View {
+    VStack {
+      AvatarView(url: Self.url, config: .account)
+        .environment(Theme.shared)
+      Toggle("Avatar Shape", isOn: $isCircleAvatar)
+    }
+    .onChange(of: isCircleAvatar) {
+      Theme.shared.avatarShape = self.isCircleAvatar ? .circle : .rounded
+    }
+    .onAppear {
+      Theme.shared.avatarShape = self.isCircleAvatar ? .circle : .rounded
+    }
   }
 
   private static let url = URL(string: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2014/03/25/12/eiffel.jpg")!
