ad2adadf87ed3ce8b98f2dfd0a538114c837b326
Thomas Durand <Dean151@users.noreply.github.com>
2023-12-03 13:54:57 +0100

    Added a "Continues a thread" label to status rows (#1704)

--- Packages/Status/Sources/Status/Row/Subviews/StatusRowReplyView.swift
+++ Packages/Status/Sources/Status/Row/Subviews/StatusRowReplyView.swift
@@ -5,26 +5,34 @@ struct StatusRowReplyView: View {
   let viewModel: StatusRowViewModel
 
   var body: some View {
-    if let accountId = viewModel.status.inReplyToAccountId,
-       let mention = viewModel.status.mentions.first(where: { $0.id == accountId })
-    {
-      HStack(spacing: 2) {
-        Image(systemName: "arrowshape.turn.up.left.fill")
-        Text("status.row.was-reply")
-        Text(mention.username)
-      }
-      .accessibilityElement(children: .combine)
-      .accessibilityLabel(
-        Text("status.row.was-reply")
-          + Text(" ")
-          + Text(mention.username)
-      )
-      .font(.scaledFootnote)
-      .foregroundColor(.gray)
-      .fontWeight(.semibold)
-      .onTapGesture {
-        viewModel.navigateToMention(mention: mention)
+    Group {
+      if let accountId = viewModel.status.inReplyToAccountId {
+        if let mention = viewModel.status.mentions.first(where: { $0.id == accountId }) {
+          HStack(spacing: 2) {
+            Image(systemName: "arrowshape.turn.up.left.fill")
+            Text("status.row.was-reply \(mention.username)")
+          }
+          .accessibilityElement(children: .combine)
+          .accessibilityLabel(
+            Text("status.row.was-reply \(mention.username)")
+          )
+          .onTapGesture {
+            viewModel.navigateToMention(mention: mention)
+          }
+        } else if viewModel.isThread && accountId == viewModel.status.account.id {
+          HStack(spacing: 2) {
+            Image(systemName: "quote.opening")
+            Text("status.row.is-thread")
+          }
+          .accessibilityElement(children: .combine)
+          .accessibilityLabel(
+            Text("status.row.is-thread")
+          )
+        }
       }
     }
+    .font(.scaledFootnote)
+    .foregroundColor(.gray)
+    .fontWeight(.semibold)
   }
 }
