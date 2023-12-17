038d029022e1722683b556a4e13ec1143248f23a
sh95014 <95387068+sh95014@users.noreply.github.com>
2023-12-16 22:33:55 -0800

    group slider and text of "max reply indentation" setting (#1749)

--- IceCubesApp/App/Tabs/Settings/DisplaySettingsView.swift
+++ IceCubesApp/App/Tabs/Settings/DisplaySettingsView.swift
@@ -207,13 +207,15 @@ struct DisplaySettingsView: View {
       Toggle("settings.display.pending-left", isOn: $userPreferences.pendingShownLeft)
       Toggle("settings.display.show-reply-indentation", isOn: $userPreferences.showReplyIndentation)
       if userPreferences.showReplyIndentation {
-        Slider(value: .init(get: {
-          Double(userPreferences.maxReplyIndentation)
-        }, set: { newVal in
-          userPreferences.maxReplyIndentation = UInt(newVal)
-        }), in: 1...20, step: 1)
-        Text("settings.display.max-reply-indentation-\(String(userPreferences.maxReplyIndentation))")
-          .font(.scaledBody)
+        VStack {
+          Slider(value: .init(get: {
+            Double(userPreferences.maxReplyIndentation)
+          }, set: { newVal in
+            userPreferences.maxReplyIndentation = UInt(newVal)
+          }), in: 1...20, step: 1)
+          Text("settings.display.max-reply-indentation-\(String(userPreferences.maxReplyIndentation))")
+            .font(.scaledBody)
+        }
       }
     }
     .listRowBackground(theme.primaryBackgroundColor)
