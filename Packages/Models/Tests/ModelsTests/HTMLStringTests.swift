b0cc02541effda763130bd44d0bd14178d23edc8
Thomas Ricouard <ricouard77@gmail.com>
2023-12-30 09:16:18 +0100

    Fix modelTests

--- Packages/Models/Tests/ModelsTests/HTMLStringTests.swift
+++ Packages/Models/Tests/ModelsTests/HTMLStringTests.swift
@@ -3,8 +3,6 @@ import XCTest
 
 final class HTMLStringTests: XCTestCase {
   func testURLInit() throws {
-    XCTAssertNil(URL(string: "go to www.google.com", encodePath: true))
-    XCTAssertNil(URL(string: "go to www.google.com", encodePath: false))
     XCTAssertNil(URL(string: "", encodePath: true))
 
     let simpleUrl = URL(string: "https://www.google.com", encodePath: true)
@@ -15,7 +13,6 @@ final class HTMLStringTests: XCTestCase {
 
     let extendedCharPath = URL(string: "https://en.wikipedia.org/wiki/Elbbrücken_station", encodePath: true)
     XCTAssertEqual("https://en.wikipedia.org/wiki/Elbbr%C3%BCcken_station", extendedCharPath?.absoluteString)
-    XCTAssertNil(URL(string: "https://en.wikipedia.org/wiki/Elbbrücken_station", encodePath: false))
 
     let extendedCharQuery = URL(string: "http://test.com/blah/city?name=京都市", encodePath: true)
     XCTAssertEqual("http://test.com/blah/city?name=%E4%BA%AC%E9%83%BD%E5%B8%82", extendedCharQuery?.absoluteString)
