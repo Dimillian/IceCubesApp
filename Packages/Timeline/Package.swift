7321b26f00491fe4a374f533d638323f78a1d945
Thomas Ricouard <ricouard77@gmail.com>
2024-06-11 08:08:25 +0200

    Compile on iOS 18

--- Packages/Timeline/Package.swift
+++ Packages/Timeline/Package.swift
@@ -23,7 +23,7 @@ let package = Package(
     .package(name: "StatusKit", path: "../StatusKit"),
     .package(name: "DesignSystem", path: "../DesignSystem"),
     .package(url: "https://github.com/siteline/SwiftUI-Introspect.git", from: "1.0.0"),
-    .package(url: "https://github.com/mergesort/Bodega", exact: "2.1.0"),
+    .package(url: "https://github.com/Dimillian/Bodega", branch: "main"),
   ],
   targets: [
     .target(
