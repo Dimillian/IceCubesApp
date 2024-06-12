0032c1dbf50d0b13a99b196220a22ee090291787
Thomas Ricouard <ricouard77@gmail.com>
2024-06-12 07:42:02 +0200

    Migrate to Swift Tests

--- Packages/Timeline/Tests/TimelineTests/TimelineFilterTests.swift
+++ Packages/Timeline/Tests/TimelineTests/TimelineFilterTests.swift
@@ -1,25 +1,28 @@
 import Models
 import Network
+import Testing
+import Foundation
 @testable import Timeline
-import XCTest
 
-final class TimelineFilterTests: XCTestCase {
-  func testCodableHome() throws {
-    XCTAssertTrue(try testCodableOn(filter: .home))
-    XCTAssertTrue(try testCodableOn(filter: .local))
-    XCTAssertTrue(try testCodableOn(filter: .federated))
-    XCTAssertTrue(try testCodableOn(filter: .remoteLocal(server: "me.dm", filter: .local)))
-    XCTAssertTrue(try testCodableOn(filter: .tagGroup(title: "test", tags: ["test"], symbolName: nil)))
-    XCTAssertTrue(try testCodableOn(filter: .tagGroup(title: "test", tags: ["test"], symbolName: "test")))
-    XCTAssertTrue(try testCodableOn(filter: .hashtag(tag: "test", accountId: nil)))
-    XCTAssertTrue(try testCodableOn(filter: .list(list: .init(id: "test", title: "test"))))
-  }
+@Test
+func testTimelineCodableHome() {
+  #expect(testCodableOn(filter: .home))
+  #expect(testCodableOn(filter: .local))
+  #expect(testCodableOn(filter: .federated))
+  #expect(testCodableOn(filter: .remoteLocal(server: "me.dm", filter: .local)))
+  #expect(testCodableOn(filter: .tagGroup(title: "test", tags: ["test"], symbolName: nil)))
+  #expect(testCodableOn(filter: .tagGroup(title: "test", tags: ["test"], symbolName: "test")))
+  #expect(testCodableOn(filter: .hashtag(tag: "test", accountId: nil)))
+  #expect(testCodableOn(filter: .list(list: .init(id: "test", title: "test"))))
+}
 
-  private func testCodableOn(filter: TimelineFilter) throws -> Bool {
-    let encoder = JSONEncoder()
-    let decoder = JSONDecoder()
-    let data = try encoder.encode(filter)
-    let newFilter = try decoder.decode(TimelineFilter.self, from: data)
-    return newFilter == filter
+fileprivate func testCodableOn(filter: TimelineFilter) -> Bool {
+  let encoder = JSONEncoder()
+  let decoder = JSONDecoder()
+  guard let data = try? encoder.encode(filter) else {
+    return false
   }
+  let newFilter = try? decoder.decode(TimelineFilter.self, from: data)
+  return newFilter == filter
+
 }
