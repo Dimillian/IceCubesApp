3c5c9adc03226d8c9c361e285d320327b23b87f4
Thomas Ricouard <ricouard77@gmail.com>
2024-07-10 16:13:41 +0200

    Cache tag + gradient on home timeline

--- Packages/StatusKit/Sources/StatusKit/Detail/StatusDetailView.swift
+++ Packages/StatusKit/Sources/StatusKit/Detail/StatusDetailView.swift
@@ -137,13 +137,6 @@ public struct StatusDetailView: View {
             }
           }
         }
-      #if !os(visionOS)
-        .listRowBackground(viewModel.highlightRowColor)
-      #endif
-        .listRowInsets(.init(top: 12,
-                             leading: .layoutPadding,
-                             bottom: 12,
-                             trailing: .layoutPadding))
     }
   }
 
