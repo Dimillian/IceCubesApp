a0286bba27046cf04c0425b3eb8a428abfb9b5d1
fwcd <fwcd@fwcd.dev>
2024-08-03 04:44:18 +0200

    Remove accidental reformatting

--- Packages/StatusKit/Sources/StatusKit/List/StatusesListView.swift
+++ Packages/StatusKit/Sources/StatusKit/List/StatusesListView.swift
@@ -29,9 +29,7 @@ public struct StatusesListView<Fetcher>: View where Fetcher: StatusesFetcher {
     switch fetcher.statusesState {
     case .loading:
       ForEach(Status.placeholders()) { status in
-        StatusRowView(viewModel: .init(status: status,
-                                       client: client,
-                                       routerPath: routerPath),
+        StatusRowView(viewModel: .init(status: status, client: client, routerPath: routerPath),
                       context: .timeline)
           .redacted(reason: .placeholder)
           .allowsHitTesting(false)
