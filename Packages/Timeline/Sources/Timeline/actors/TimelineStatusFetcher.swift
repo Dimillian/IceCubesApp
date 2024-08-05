6c9d9161dcbd382cb70e3eb17040c8e91c4476fd
Thomas Ricouard <ricouard77@gmail.com>
2024-08-05 13:59:38 +0200

    Extract function from TimelineViewModel

---
+++ Packages/Timeline/Sources/Timeline/actors/TimelineStatusFetcher.swift
@@ -0,0 +1,60 @@
+import Foundation
+import Models
+import Network
+
+protocol TimelineStatusFetching: Sendable {
+  func fetchFirstPage(client: Client?,
+                      timeline: TimelineFilter) async throws -> [Status]
+  func fetchNewPages(client: Client?,
+                     timeline: TimelineFilter,
+                     minId: String,
+                     maxPages: Int) async throws -> [Status]
+  func fetchNextPage(client: Client?,
+                     timeline: TimelineFilter,
+                     lastId: String,
+                     offset: Int) async throws -> [Status]
+}
+
+enum StatusFetcherError: Error {
+  case noClientAvailable
+}
+
+struct TimelineStatusFetcher: TimelineStatusFetching {
+  func fetchFirstPage(client: Client?, timeline: TimelineFilter) async throws -> [Status] {
+    guard let client = client else { throw StatusFetcherError.noClientAvailable }
+    return try await client.get(endpoint: timeline.endpoint(sinceId: nil,
+                                                            maxId: nil,
+                                                            minId: nil,
+                                                            offset: 0))
+  }
+  
+  func fetchNewPages(client: Client?, timeline: TimelineFilter, minId: String, maxPages: Int) async throws -> [Status] {
+    guard let client = client else { throw StatusFetcherError.noClientAvailable }
+    var allStatuses: [Status] = []
+    var latestMinId = minId
+    for _ in 1...maxPages {
+      if Task.isCancelled { break }
+      
+      let newStatuses: [Status] = try await client.get(endpoint: timeline.endpoint(
+        sinceId: nil,
+        maxId: nil,
+        minId: latestMinId,
+        offset: nil
+      ))
+      
+      if newStatuses.isEmpty { break }
+      
+      allStatuses.insert(contentsOf: newStatuses, at: 0)
+      latestMinId = newStatuses.first?.id ?? latestMinId
+    }
+    return allStatuses
+  }
+  
+  func fetchNextPage(client: Client?, timeline: TimelineFilter, lastId: String, offset: Int) async throws -> [Status] {
+    guard let client = client else { throw StatusFetcherError.noClientAvailable }
+    return try await client.get(endpoint: timeline.endpoint(sinceId: nil,
+                                                            maxId: lastId,
+                                                            minId: nil,
+                                                            offset: offset))
+  }
+}
