da8cb99ee1042d9984faf6e90ddd9c68791cf6e4
Thomas Ricouard <ricouard77@gmail.com>
2024-08-13 12:41:23 +0200

    Cleanup

--- Packages/StatusKit/Sources/StatusKit/Editor/ViewModel.swift
+++ Packages/StatusKit/Sources/StatusKit/Editor/ViewModel.swift
@@ -17,7 +17,7 @@ public extension StatusEditor {
     var client: Client?
     var currentAccount: Account? {
       didSet {
-        if let itemsProvider {
+        if itemsProvider != nil {
           mediaContainers = []
         }
       }
