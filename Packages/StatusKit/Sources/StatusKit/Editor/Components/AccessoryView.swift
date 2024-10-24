390673800eaecfde959349628821a491334e3c83
Thomas Ricouard <ricouard77@gmail.com>
2024-10-24 13:54:16 +0100

    Fix crash on emoji picket on macOS

--- Packages/StatusKit/Sources/StatusKit/Editor/Components/AccessoryView.swift
+++ Packages/StatusKit/Sources/StatusKit/Editor/Components/AccessoryView.swift
@@ -179,6 +179,7 @@ extension StatusEditor {
         .accessibilityLabel("accessibility.editor.button.custom-emojis")
         .sheet(isPresented: $isCustomEmojisSheetDisplay) {
           CustomEmojisView(viewModel: focusedSEVM)
+            .environment(theme)
         }
       }
 
