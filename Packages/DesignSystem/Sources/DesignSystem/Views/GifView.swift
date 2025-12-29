c7441883afc92d769388889c439d662d224304d9
Thomas Ricouard <ricouard77@gmail.com>
2025-12-29 09:19:35 +0100

    Fixes

--- Packages/DesignSystem/Sources/DesignSystem/Views/GifView.swift
+++ Packages/DesignSystem/Sources/DesignSystem/Views/GifView.swift
@@ -5,42 +5,44 @@
 //  Created by Robert George on 10/26/25.
 //
 
-import SwiftUI
-import Gifu
 import Env
+import Gifu
+import SwiftUI
 
 public struct GifView: UIViewRepresentable {
-    let animatedView = GIFImageView()
-    var data : Data
-    @Environment(UserPreferences.self) private var preferences
-    @Environment(Theme.self) private var theme
+  @Environment(UserPreferences.self) private var preferences
+  @Environment(Theme.self) private var theme
 
-    public init(data: Data) {
-        self.data = data
-    }
-    
-    public func makeUIView(context: UIViewRepresentableContext<GifView>) -> UIView {
-        let view = UIView()
-        
-        animatedView.prepareForAnimation(withGIFData: data)
+  let animatedView = GIFImageView()
+  var data: Data
 
-        animatedView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(animatedView)
-        
-        if theme.avatarAnimated {
-            animatedView.startAnimatingGIF()
-        }
-        
-        NSLayoutConstraint.activate([
-            animatedView.heightAnchor.constraint(equalTo: view.heightAnchor),
-            animatedView.widthAnchor.constraint(equalTo: view.widthAnchor)
-        ])
-        
-        return view
+  public init(data: Data) {
+    self.data = data
+  }
+
+  public func makeUIView(context: UIViewRepresentableContext<GifView>) -> UIView {
+    let view = UIView()
+
+    animatedView.prepareForAnimation(withGIFData: data)
+    animatedView.contentMode = .scaleAspectFit
+
+    animatedView.translatesAutoresizingMaskIntoConstraints = false
+    view.addSubview(animatedView)
+
+    if theme.avatarAnimated {
+      animatedView.startAnimatingGIF()
     }
-    
-    public func updateUIView(_ uiView: UIView, context: Context) {
-        
-    }
+
+    NSLayoutConstraint.activate([
+      animatedView.heightAnchor.constraint(equalTo: view.heightAnchor),
+      animatedView.widthAnchor.constraint(equalTo: view.widthAnchor),
+    ])
+
+    return view
+  }
+
+  public func updateUIView(_ uiView: UIView, context: Context) {
+
+  }
 
 }
