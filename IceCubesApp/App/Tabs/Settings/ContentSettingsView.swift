01e37830f2e1865e9c87525b3f8dac65d7d4d055
dushyant <devdushyantatwork@gmail.coom>
2026-01-02 19:04:50 +0530

    feat: implement media filtering logic and fix formatting (#2382)

--- IceCubesApp/App/Tabs/Settings/ContentSettingsView.swift
+++ IceCubesApp/App/Tabs/Settings/ContentSettingsView.swift
@@ -28,8 +28,8 @@ struct ContentSettingsView: View {
         Toggle(isOn: $userPreferences.showAltTextForMedia) {
           Text("settings.content.media.show.alt")
         }
-          Toggle(isOn: $userPreferences.animateEmojis) {
-              Text("settings.other.animate-emojis")
+        Toggle(isOn: $userPreferences.animateEmojis) {
+          Text("settings.other.animate-emojis")
         }
         Toggle(isOn: $userPreferences.hidePostsWithMedia) {
           Text("settings.content.media.hide-posts-with-media")
